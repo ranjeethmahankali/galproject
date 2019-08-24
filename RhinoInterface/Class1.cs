31991457812b4a812e906626589adcf502b6738d
Ranjeeth Mahankali <ranjeethmahankali@gmal.com>
2019-08-24 20:28:55 +0200

    Add the empty .net and cpp projects

---
+++ RhinoInterface/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace RhinoInterface
+{
+    public class Class1
+    {
+
+    }
+}
