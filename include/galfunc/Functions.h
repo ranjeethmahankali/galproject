ef8fc0d20043df17892858315c3ece2ede33b05e
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-03-21 21:56:11 +0100

    Try adding interfaces

--- include/galfunc/Functions.h
+++ include/galfunc/Functions.h
@@ -1,13 +1,30 @@
 #pragma once
 
-#include <galfunc/MapMacro.h>
 #include <string.h>
-#include <boost/python.hpp>
 #include <functional>
-#include <glm/glm.hpp>
 
 namespace gal {
 namespace func {
+
+class Session
+{
+};
+
+class IFunctor;
+
+struct IDataRef
+{
+  virtual void*    data()   = 0;
+  virtual uint32_t typeId() = 0;
+  virtual IFunctor* owner()  = 0;
+};
+
+class IFuctor
+{
+public:
+  virtual std::string name() const = 0;
+  virtual void        run()        = 0;
+};
 
 }  // namespace func
 }  // namespace gal
