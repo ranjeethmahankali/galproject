5578ec24a09fdaf9d62c79ab9d08ad7427c2e409
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-03-24 02:55:31 +0100

    Use templates for sliders

--- src/galview/Widget.cpp
+++ src/galview/Widget.cpp
@@ -1,3 +1,4 @@
+
 #include <galview/Widget.h>
 #include <iostream>
 
@@ -49,40 +50,6 @@ Text::Text(const std::string& value)
 void Text::draw()
 {
   ImGui::Text(mValue.c_str());
-};
-
-Slider::Slider(const std::string& label, float min, float max)
-    : InputWidget<float>(label, std::clamp(0.0f, min, max))
-    , mRange {min, max} {};
-
-Slider::Slider(const std::string& label, float min, float max, float value)
-    : InputWidget<float>(label, std::clamp(value, min, max))
-    , mRange {min, max} {};
-
-void Slider::draw()
-{
-  ImGui::SliderFloat(mLabel.c_str(), &mValue, mRange[0], mRange[1]);
-  handleChanges();
-};
-
-Slider3::Slider3(const std::string& label, float min, float max)
-    : InputWidget<float[3]>(label)
-    , mRange {min, max}
-{
-  std::fill_n(mValue, 3, std::clamp(0.0f, mRange[0], mRange[1]));
-};
-
-Slider3::Slider3(const std::string& label, float min, float max, const float (&value)[3])
-    : InputWidget<float[3]>(label)
-    , mRange {min, max}
-{
-  std::copy_n(value, 3, mValue);
-};
-
-void Slider3::draw()
-{
-  ImGui::SliderFloat3(mLabel.c_str(), mValue, mRange[0], mRange[1]);
-  handleChanges();
 };
 
 }  // namespace view
