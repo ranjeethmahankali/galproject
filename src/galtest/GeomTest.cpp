d159cb529444e0da7970c6f4d31f4b5331d11ef4
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-07-04 17:30:39 +0200

    Add another test case

--- src/galtest/GeomTest.cpp
+++ src/galtest/GeomTest.cpp
@@ -6,21 +6,30 @@
 TEST(Circle2d, MinBoundingCircle)
 {
   GALSCOPE(__func__);
-  std::vector<glm::vec2> points {
-    {0.f, 0.f},
-    {1.f, 0.f},
-    {0.25f, 0.25f},
-    {0.9f, 0.9f},
-    {-.3f, 1.f},
-    {0.f, -1.f},
-  };
+  const static std::vector<std::vector<glm::vec2>> pointSets {{
+                                                                {0.f, 0.f},
+                                                                {1.f, 0.f},
+                                                                {0.25f, 0.25f},
+                                                                {1.f, 1.f},
+                                                                {-.3f, 1.f},
+                                                              },
+                                                              {
+                                                                {1.f, 0.f},
+                                                                {0.f, 0.f},
+                                                                {3.f, 0.f},
+                                                                {2.f, 0.f},
+                                                                {4.f, 0.f},
+                                                                {5.f, 0.f},
+                                                              }};
 
-  auto cloud = gal::PointCloud(points);
-  GALWATCH(cloud);
-  GALCAPTURE(gal::Annotations::createIndexedPointCloud(cloud), tags);
-  auto circ = gal::Circle2d::minBoundingCircle(points);
+  for (const auto& points : pointSets) {
+    auto cloud = gal::PointCloud(points);
+    GALWATCH(cloud);
+    GALCAPTURE(gal::Annotations::createIndexedPointCloud(cloud), tags);
+    auto circ = gal::Circle2d::minBoundingCircle(points);
 
-  for (const auto& pt : points) {
-    ASSERT_TRUE(circ.contains(pt));
+    for (const auto& pt : points) {
+      ASSERT_TRUE(circ.contains(pt));
+    }
   }
 }
