c39a226969367ad054cf25a76a58628c80f0ff64
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-07-09 00:47:57 +0200

    Restore file from master

--- demos/meshView.py
+++ demos/meshView.py
@@ -1,14 +1,10 @@
 import pygalfunc as pgf
 import pygalview as pgv
 
-relpath, = pgf.string("../assets/bunny_large.obj")
-# relpath, = pgv.textField("Relative file path");
+# relpath, = pgf.string("../assets/bunny_large.obj")
+relpath, = pgv.textField("Relative file path");
 path, = pgf.absPath(relpath)
 mesh, = pgf.loadObjFile(path)
 box, = pgf.meshBbox(mesh)
-cent, = pgf.meshCentroid(mesh)
-
-print(pgf.read(cent))
-
 pgv.show("mesh", mesh)
 pgv.show("bounds", box)
