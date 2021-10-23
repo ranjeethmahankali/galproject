40a8c416fbd38b024b360f048ac9a4a5880283e5
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-10-23 23:35:30 +0200

    Try fix bug

--- src/galtest/ObjLoaderTest.cpp
+++ src/galtest/ObjLoaderTest.cpp
@@ -10,6 +10,7 @@ TEST(ObjLoader, DISABLED_DragonMesh)
 #endif
 {
   auto path = GAL_ASSET_DIR / "dragon.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(435545, mesh.numVertices());
   ASSERT_EQ(871306, mesh.numFaces());
@@ -18,6 +19,7 @@ TEST(ObjLoader, DISABLED_DragonMesh)
 TEST(ObjLoader, BunnyLarge)
 {
   auto path = GAL_ASSET_DIR / "bunny_large.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(34817, mesh.numVertices());
   ASSERT_EQ(69630, mesh.numFaces());
@@ -26,6 +28,7 @@ TEST(ObjLoader, BunnyLarge)
 TEST(ObjLoader, BunnySmall)
 {
   auto path = GAL_ASSET_DIR / "bunny.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(2503, mesh.numVertices());
   ASSERT_EQ(4968, mesh.numFaces());
