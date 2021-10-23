71b2847f424d6502f9e63897dcc1283072c0ad24
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-10-23 16:30:18 +0200

    Support for Overloaded Functions (#28)
    * Refactor to create python binding in a different way
    
    * Implement overloads and create a new demo to test it
    
    * Misc clean up
    
    * Add support for overloads through template instantiations
    
    * Mist comments and clean up

---
+++ demos/overloads.py
@@ -0,0 +1,44 @@
+import pygalfunc as pgf
+import pygalview as pgv
+
+def sphere():
+    minCoord = pgf.var(-1.)
+    maxCoord = pgf.var(1.)
+    minpt = pgf.vec3(minCoord, minCoord, minCoord)
+    maxpt = pgf.vec3(maxCoord, maxCoord, maxCoord)
+    box = pgf.box3(minpt, maxpt)
+    npts = pgv.slideri32("Point count", 5, 50, 25)
+    cloud = pgf.randomPointsInBox(box, npts)
+    pgv.show("Point Cloud", cloud)
+    sphere, *_ = pgf.boundingSphere(cloud)
+    return sphere
+
+def mesh():
+    relpath = pgf.var("../assets/bunny_large.obj")
+    # relpath = pgv.textField("Relative file path");
+    path = pgf.absPath(relpath)
+    return pgf.loadObjFile(path)
+
+m = mesh()
+s = sphere()
+
+# The bounds function is overloaded for both mesh and sphere.
+meshbbox = pgf.bounds(m)
+spbbox = pgf.bounds(s)
+
+pgv.show("mesh", m)
+pgv.show("circ", s)
+pgv.show("mbbox", meshbbox)
+pgv.show("cbbox", spbbox)
+
+ia = pgv.slideri32("int a", 5, 50, 25);
+ib = pgv.slideri32("int b", 5, 50, 25);
+fa = pgv.sliderf32("float a", 5., 50., 25.);
+fb = pgv.sliderf32("float b", 5., 50., 25.);
+
+# add function is overloaded for both floats and ints.
+fsum = pgf.add(fa, fb)
+isum = pgf.add(ia, ib)
+
+pgv.print("fsum", fsum)
+pgv.print("isum", isum)
--- src/galfunc/CircleFunctions.cpp
+++ src/galfunc/CircleFunctions.cpp
@@ -1,11 +1,15 @@
-#include <galfunc/CircleFunctions.h>
+#include <galfunc/Functions.h>
 
 namespace gal {
 namespace func {
 
-GAL_FUNC_DEFN(boundingCircle,
-              (((data::ReadView<glm::vec3, 1>), points)),
-              ((gal::Circle2d, circle), (glm::vec2, center), (float, radius)))
+GAL_FUNC(boundingCircle,
+         "Creates a bounding circle for the given points. The 3d points are "
+         "flattened to 2d by removing the z-coordinate.",
+         (((data::ReadView<glm::vec3, 1>), points, "Points")),
+         ((gal::Circle2d, circle, "Bounding circle"),
+          (glm::vec2, center, "Center of the circle"),
+          (float, radius, "Radius of the circle")))
 {
   // TODO: Clean this up to consume 2d points as inputs directly.
   std::vector<glm::vec2> pts2d;
@@ -19,28 +23,75 @@ GAL_FUNC_DEFN(boundingCircle,
   radius = circle.radius();
 }
 
-GAL_FUNC_DEFN(circle2d, ((glm::vec2, center), (float, radius)), ((gal::Circle2d, circle)))
+GAL_FUNC(circle2d,
+         "Creates a bounding circle for the given points. The 3d points are "
+         "flattened to 2d by removing the z-coordinate.",
+         ((glm::vec2, center, "Center"), (float, radius, "Radius")),
+         ((gal::Circle2d, circle, "Circle")))
 {
   circle.center(center);
   circle.radius(radius);
 }
 
-GAL_FUNC_DEFN(circle2dFromDiameter,
-              ((glm::vec2, pt1), (glm::vec2, pt2)),
-              ((gal::Circle2d, circle), (glm::vec2, center), (float, radius)))
+GAL_FUNC(circle2dFromDiameter,
+         "Creates a 2d circle with the given points as the ends of the diameter",
+         ((glm::vec2, pt1, "First point"), (glm::vec2, pt2, "Second point")),
+         ((gal::Circle2d, circle, "Circle"),
+          (glm::vec2, center, "Center of the circle"),
+          (float, radius, "Radius of the circle")))
 {
   circle = gal::Circle2d::createFromDiameter(pt1, pt2);
   center = circle.center();
   radius = circle.radius();
 }
 
-GAL_FUNC_DEFN(circumCircle2d,
-              ((glm::vec2, pt1), (glm::vec2, pt2), (glm::vec2, pt3)),
-              ((gal::Circle2d, circle), (glm::vec2, center), (float, radius)))
+GAL_FUNC(circumCircle2d,
+         "Creates a 2d circle with the given points as the ends of the diameter",
+         ((glm::vec2, pt1, "First point"),
+          (glm::vec2, pt2, "Second point"),
+          (glm::vec2, pt3, "Third point")),
+         ((gal::Circle2d, circle, "Circle"),
+          (glm::vec2, center, "Center of the circle"),
+          (float, radius, "Radius of the circle")))
 {
   circle = gal::Circle2d::createCircumcircle(pt1, pt2, pt3);
   center = circle.center();
   radius = circle.radius();
+}
+
+GAL_FUNC(bounds,
+         "Bounding box of the circle",
+         ((gal::Circle2d, circ, "The circle")),
+         ((gal::Box2, bbox, "Bounding box")))
+{
+  bbox = circ.bounds();
+}
+
+GAL_FUNC(area,
+         "Area of the circle",
+         ((gal::Circle2d, circ, "The circle")),
+         ((float, result, "Area")))
+{
+  result = circ.area();
+}
+
+GAL_FUNC(perimeter,
+         "Perimter of the circle",
+         ((gal::Circle2d, circ, "The circle")),
+         ((float, result, "Perimeter")))
+{
+  result = circ.perimeter();
+}
+
+void bind_CircleFunctions()
+{
+  GAL_FN_BIND(bounds);
+  GAL_FN_BIND(area);
+
+  GAL_FN_BIND(boundingCircle);
+  GAL_FN_BIND(circle2d);
+  GAL_FN_BIND(circle2dFromDiameter);
+  GAL_FN_BIND(circumCircle2d);
 }
 
 }  // namespace func
