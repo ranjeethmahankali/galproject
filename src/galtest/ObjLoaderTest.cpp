74753d4021315086e26844cff33ac3d70de1e3b5
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2021-10-24 09:07:31 +0200

    Testing and CI (#30)
    * Create cmake.yml
    
    * Misc update
    
    * Try automate vcpkg dependencies
    
    * Try caching vcpkg packages
    
    * Try fix problem
    
    * Try fix bug

--- src/galtest/ObjLoaderTest.cpp
+++ src/galtest/ObjLoaderTest.cpp
@@ -1,5 +1,7 @@
-#include <galcore/ObjLoader.h>
 #include <gtest/gtest.h>
+
+#include <galcore/ObjLoader.h>
+#include <galtest/TestUtils.h>
 
 #ifdef NDEBUG
 TEST(ObjLoader, DragonMesh)
@@ -7,7 +9,8 @@ TEST(ObjLoader, DragonMesh)
 TEST(ObjLoader, DISABLED_DragonMesh)
 #endif
 {
-  auto path = gal::utils::absPath("../assets/dragon.obj");
+  auto path = GAL_ASSET_DIR / "dragon.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(435545, mesh.numVertices());
   ASSERT_EQ(871306, mesh.numFaces());
@@ -15,7 +18,8 @@ TEST(ObjLoader, DISABLED_DragonMesh)
 
 TEST(ObjLoader, BunnyLarge)
 {
-  auto path = gal::utils::absPath("../assets/bunny_large.obj");
+  auto path = GAL_ASSET_DIR / "bunny_large.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(34817, mesh.numVertices());
   ASSERT_EQ(69630, mesh.numFaces());
@@ -23,7 +27,8 @@ TEST(ObjLoader, BunnyLarge)
 
 TEST(ObjLoader, BunnySmall)
 {
-  auto path = gal::utils::absPath("../assets/bunny.obj");
+  auto path = GAL_ASSET_DIR / "bunny.obj";
+  std::cout << "Loading mesh from " << path << std::endl;
   auto mesh = gal::io::ObjMeshData(path).toMesh();
   ASSERT_EQ(2503, mesh.numVertices());
   ASSERT_EQ(4968, mesh.numFaces());
