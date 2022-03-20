df8af45c87008d22b09ac706ed339ab2f0bb68b2
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2022-03-20 14:52:09 +0100

    Pin imnodes version

--- external.cmake
+++ external.cmake
@@ -6,6 +6,7 @@ set(EXTERNAL_DIR ${CMAKE_SOURCE_DIR}/external)
 message("Cloning imnodes into " ${EXTERNAL_DIR}/imnodes)
 FetchContent_Declare(imnodes
   GIT_REPOSITORY https://github.com/Nelarius/imnodes.git
+  GIT_TAG 8433b51a911ad200482affe2cfd0e07a46de303c
   PREFIX ${EXTERNAL_DIR}
   SOURCE_DIR ${EXTERNAL_DIR}/imnodes
   TMP_DIR ${EXTERNAL_DIR}/temp
