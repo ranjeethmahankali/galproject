d64e45aff8f0b11be520aa5c4514e9899b5bc5b9
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2022-07-02 12:40:08 -0400

    More debugging

--- demos/meshWithColors.py
+++ demos/meshWithColors.py
@@ -1,7 +1,6 @@
 import pygalfunc as pgf
 import pygalview as pgv
 
-
 pt = pgf.var_vec3((0., 0., 0.))
 norm = pgf.var_vec3((0., 0., 1.))
 plane = pgf.plane(pt, norm)
@@ -9,22 +8,19 @@ minpt = pgf.var_vec3((-.5, -.5, 0.))
 maxpt = pgf.var_vec3((.5, .5, 0.))
 box2 = pgf.box2(pgf.var_vec2((-.5, -.5)), pgf.var_vec2((.5, .5)))
 box3 = pgf.box3(minpt, maxpt)
-npts = pgv.slideri32("Point count", 5, 50, 25)
+# npts = pgv.slideri32("Point count", 5, 50, 25)
+npts = pgf.var_int(2)
 cloud = pgf.randomPointsInBox(box3, npts)
 
-edgeLen = pgf.var_float(.01)
+edgeLen = pgf.var_float(1)
 rect = pgf.rectangleMesh(plane, box2, edgeLen)
-
-distances = pgf.distance(pgf.graft(pgf.vertices(rect)), cloud)
+vertices = pgf.vertices(rect)
+distances = pgf.distance(pgf.graft(vertices), cloud)
 sortedDists = pgf.sort(distances, distances)
-maxDist = pgf.listItem(sortedDists, pgf.sub(pgf.listLength(sortedDists), pgf.var_int(1)))
+maxDist = pgf.listItem(sortedDists,
+                       pgf.sub(pgf.listLength(sortedDists), pgf.var_int(1)))
 
-scheme = pgf.var_vec3([
-    (0., 0., 1.),
-    (0., 1., 0.),
-    (1., 1., 0.),
-    (1., 0., 0.)
-])
+scheme = pgf.var_vec3([(0., 0., 1.), (0., 1., 0.), (1., 1., 0.), (1., 0., 0.)])
 colors = pgf.mapValueToColor(maxDist, pgf.var_vec2((.5, 1.2)), scheme)
 colored = pgf.meshWithVertexColors(rect, colors)
 
