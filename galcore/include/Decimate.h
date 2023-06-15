6d22b901732801b5e531b413680f2702f0539c91
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-06-14 23:37:37 -0400

    Misc changes

--- galcore/include/Decimate.h
+++ galcore/include/Decimate.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <Mesh.h>
+#include <OpenMesh/Tools/Decimater/DecimaterT.hh>
+#include <OpenMesh/Tools/Decimater/ModProgMeshT.hh>
+#include <OpenMesh/Tools/Decimater/ModQuadricT.hh>
 
 namespace gal {
 
@@ -8,7 +11,15 @@ template<typename MeshOutIter>
 void decimateWithHistory(TriMesh mesh, MeshOutIter outmeshes)
 {
   *(outmeshes++) = mesh;
-  *(outmeshes++) = mesh;
+  OpenMesh::Decimater::DecimaterT<TriMesh>          decimater(mesh);
+  OpenMesh::Decimater::ModQuadricT<TriMesh>::Handle quadric;
+  // OpenMesh::Decimater::ModProgMeshT<TriMesh>::Handle prog;
+  decimater.add(quadric);
+  // decimater.add(prog);
+  decimater.module(quadric).set_max_err(0.0001);
+  decimater.initialize();
+  decimater.decimate();
+  *(outmeshes++) = decimater.mesh();
   // TODO: Not implemented.
 }
 
