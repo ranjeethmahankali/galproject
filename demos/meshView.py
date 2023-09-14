f93d0112a22cd1a588b6ad788fc90d7cf2bcd3f5
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-09-13 22:45:59 -0400

    Misc change

--- demos/meshView.py
+++ demos/meshView.py
@@ -2,19 +2,15 @@
 import pygalfunc as pgf
 import pygalview as pgv
 
-# tmesh = pgf.loadTriangleMesh(
-#     pgf.absPath(pgf.var_string("../../assets/bunny_large.obj")))
-# pgv.show("bunny", tmesh)
-# pgv.show("bunnyBounds", pgf.bounds(tmesh))
-# pgv.show("bunnyCentroid", pgf.centroid(tmesh))
-# qmesh = pgf.scale(
-#     pgf.loadPolyMesh(pgf.absPath(pgf.var_string("../../assets/cane.obj"))),
-#     pgf.var_float(0.1))
-# pgv.show("cane", qmesh)
-
-pgv.show(
-    "test",
-    pgf.loadPolyMesh(pgf.var_string("/home/rnjth94/downloads/quadsphere.obj")))
+tmesh = pgf.loadTriangleMesh(
+    pgf.absPath(pgf.var_string("../../assets/bunny_large.obj")))
+pgv.show("bunny", tmesh)
+pgv.show("bunnyBounds", pgf.bounds(tmesh))
+pgv.show("bunnyCentroid", pgf.centroid(tmesh))
+qmesh = pgf.scale(
+    pgf.loadPolyMesh(pgf.absPath(pgf.var_string("../../assets/cane.obj"))),
+    pgf.var_float(0.1))
+pgv.show("cane", qmesh)
 
 pgv.runCommands("""
 meshedges on
