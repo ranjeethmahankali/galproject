6534d1bb32996a9d4b6ae9cc74df26170cf759db
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-02-11 22:44:43 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
-CONFIG=${1-build}
+#!/bin/bash
+
+CONFIG=${1-Release}
 TARGET=${2-all}
 NUM_THREADS=${3-12}
-mkdir -p $CONFIG
-cd $CONFIG
-echo "Building target '${TARGET}' in ${CONFIG} configuration"
 
-# This is the default version.
-if [ $CONFIG == "build" ]; then
-    echo "Generating compilation commands..."
-    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
-    cp ./compile_commands.json ../
-else
-    cmake -DCMAKE_BUILD_TYPE=$CONFIG ..
-    printf "Starting build using %s threads...\n" $NUM_THREADS
-    cmake --build . --config $CONFIG --target $TARGET -j $NUM_THREADS
-fi
+
+mkdir -p build
+cd build
+
+echo "Generating compilation commands..."
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+cp ./compile_commands.json ../
+
+echo "Generating for project..."
+cmake ..
+
+# Build with half the threads available on the computer.
+nthreads=$(echo $(($(nproc) / 2)))
+printf "Starting build using %s threads...\n" $nthreads
+cmake --build . --config $CONFIG --target $TARGET -j $nthreads
